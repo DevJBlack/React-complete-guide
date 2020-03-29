dff745ffeee6bcacc2ec44d59a21376e452de596
Devin Black <Devinjamesblack@gmail.com>
2020-03-29 07:31:29 -0600

    class component

--- src/App.js
+++ src/App.js
@@ -1,73 +1,179 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './sass/App.scss'
-
 import Person from './Person/Person'
 
-const App = () => {
-  const [personState, setPersonState] = useState([
-      {name: 'Max', age: 28},
-      {name: 'Devin', age: 24},
-      {name: 'Jesse', age: 24}
-    ]);
-    // const [otherState] = useState('some other value')
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'a', name: 'Max', age: 28 },
+      { id: 'b', name: 'Devin', age: 24 },
+      { id: 'c', name: 'Jesse', age: 24 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
 
-    const [showPersons, setShowPersons] = useState(false)
+  nameChangedHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value
+    const persons = [...this.state.persons]
+    persons[personIndex] = person;
 
 
-    const togglePeronHandler = () => {
-      if (showPersons === true) {
-        return(setShowPersons(false))
-      } else {
-        return(setShowPersons(true))
-      }
-    }
+    this.setState( { persons: persons } )
+  }
 
-   
-    const nameChangedHandler = (e) => {
-      return (
-        setPersonState([
-            {name: 'Max', age: 28},
-            {name: e.target.value, age: 24},
-            {name: 'Jesse', age: 24}
-            
-          ]
-        )
-      )
-    }
+  deletePersonHandler = ( personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
 
-    const deletePersonHandler = (personIndex) => {
-      // const persons = personState.slice();
-      const persons = [...personState]
-      persons.splice(personIndex, 1);
-      setPersonState({persons: persons})
-      console.log(persons)
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } )
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
     }
 
     let persons = null;
-    
-    if (showPersons) {
+
+    if ( this.state.showPersons ) {
       persons = (
-        <div className="hide">
-          {personState.map((person, index) => {
+        <div>
+          {this.state.persons.map((person, index) => {
             return <Person 
-              click={() => deletePersonHandler(index)}
-              changed={nameChangedHandler}
-              person={person.name} 
-              age={person.age} />
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}
+            />
           })}
-        </div> 
+        </div>
       )
     }
 
-  return (
-    <div className="App">
-      <h1>Hello There</h1>
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}> Toggle Persons </button>
+          { persons }
+      </div>
+    )
 
-        <button onClick={ togglePeronHandler } className="btn btn-white">Toggle Person</button>
-        { persons }
+  }
 
-    </div>
-  );
+  
 }
+export default App
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import './sass/App.scss'
+// import Person from './Person/Person'
+
+// const App = () => {
+//   const [personState, setPersonState] = useState({
+//          person: [ 
+//       { id: 'a', name: 'Max', age: 28},
+//       { id: 'b', name: 'Devin', age: 24},
+//       { id: 'c', name: 'Jesse', age: 24}
+//     ]});
+    
+//     const [showPersons, setShowPersons] = useState(false)
+
+    
+    
+//     const togglePeronHandler = () => {
+//       if (showPersons === true) {
+//         return(setShowPersons(false))
+//       } else {
+//         return(setShowPersons(true))
+//       }
+//     }
+    
+   
+//     const nameChangedHandler = ( event, id ) => {
+//       const personIndex = personState.person.findIndex(p => {
+//         return p.id === id;
+//       })
+
+//       const person = {
+//         ...personState.person[personIndex]
+//       }
+      
+//       person.name = event.target.value
+//       const persons = [...personState.person]
+//       persons[personIndex] = person
+
+//         setPersonState( {persons: persons} )
+//       }
+      
+//       const deletePersonHandler = (personIndex) => {
+//         const array = [...personState.person]
+//         array.splice(personIndex, 1 );
+//         setPersonState({array})
+//         console.log(array)
+//       }
+
+      
+//       let persons = null;
+      
+//       if (showPersons) {
+//         persons = (
+//           <div>
+//           {personState.person.map((person, index) => {
+//             return <Person 
+//               click={() => deletePersonHandler(index)}
+//               name={person.name} 
+//               age={person.age}
+//               key={person.id}
+//               changed={(event) => nameChangedHandler(event, person.id)}
+//               />
+//             })}
+//           </div> 
+//       )
+//     }
+
+//   return (
+//     <div className="App">
+//       <h1>Hello There</h1>
+//         <button onClick={ togglePeronHandler } className="btn btn-white">Toggle Person</button>
+
+//         { persons }
+
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const randomAge = Math.floor(Math.random() * 30)
+// const randomAge = Math.floor(Math.random() * 30)
 
 const Person = (props) => {
   return(
    <div className="person">
-      <p onClick={props.click} >I'm {props.person} and I am {props.age} years old put sometimes I feel {randomAge} </p>
-      <input type="text" onChange={props.changed} placeholder={props.person} />
+      <p onClick={props.click} >I'm {props.name} and I am {props.age}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
    </div>
   )
 }
